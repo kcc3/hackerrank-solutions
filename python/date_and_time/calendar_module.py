2d4c6956f34125eae059c4ead413a303680899c4
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2020-04-07 21:08:36 -0700

    Calendar Module
    Solution for the hackerrank problem solving challenge: Practice -> Python -> Date and Time -> Calendar Module

---
+++ python/date_and_time/calendar_module.py
@@ -0,0 +1,9 @@
+"""
+Hackerrank Problem: https://www.hackerrank.com/challenges/calendar-module/problem
+
+Problem: Given a date. Your task is to find what the day is on that date.
+"""
+import calendar
+
+month, day, year = map(int, raw_input().split(" "))
+print calendar.day_name[calendar.weekday(year, month, day)].upper()
