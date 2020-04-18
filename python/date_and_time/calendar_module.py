627c21214660153e4b63798559d96ad3c2e40694
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2020-04-18 02:34:23 -0700

    Calendar Module
    Updating to be python 3 compliant!

--- python/date_and_time/calendar_module.py
+++ python/date_and_time/calendar_module.py
@@ -5,5 +5,5 @@ Problem: Given a date. Your task is to find what the day is on that date.
 """
 import calendar
 
-month, day, year = map(int, raw_input().split(" "))
-print calendar.day_name[calendar.weekday(year, month, day)].upper()
+month, day, year = map(int, input().split(" "))
+print(calendar.day_name[calendar.weekday(year, month, day)].upper())
