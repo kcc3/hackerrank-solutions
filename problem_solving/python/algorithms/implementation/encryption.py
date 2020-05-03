c96d49874cf9ee243f8aec70b74b250ec7a806ee
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2020-05-03 13:03:37 -0700

    Cleanup Encryption
    Removing the matrix solution and using python's great string slicing features which removes sooooooo much code.

--- problem_solving/python/algorithms/implementation/encryption.py
+++ problem_solving/python/algorithms/implementation/encryption.py
@@ -13,29 +13,11 @@ def encryption(s):
         str: The string after passing through specified encryption
     """
     string = s.replace(" ", "")
-    output = []
-    # Figure out the rows and columns based on string length
-    rows = math.floor(math.sqrt(len(string)))
     columns = math.ceil(math.sqrt(len(string)))
-    if rows * columns < len(string):
-        rows += 1
-    # Iterate through and populate into the rows and columns matrix
-    for i in range(rows):
-        chars = []
-        for j in range(columns):
-            if j + i * columns < len(string):
-                chars.append(string[j + i * columns])
-            else:
-                chars.append("")
-        output.append(chars)
-    # Now print out the string based on columns
-    result = []
+    line = ""
     for i in range(columns):
-        line = ""
-        for j in range(rows):
-            line += output[j][i]
-        result.append(line)
-    return " ".join(result)
+        line += string[i::columns] + " "
+    return line
 
 
 if __name__ == "__main__":
