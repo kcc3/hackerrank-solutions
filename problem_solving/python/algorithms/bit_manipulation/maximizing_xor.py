81b3b7fc9040beec4d56eb18f54a7292b9c7dc0c
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2020-08-17 23:18:53 -0700

    Maximizing XOR
    The more elegant solve.  Here, we just XOR the l and r value because those are the outer band within which a and b fall.  We can use that to find out the largest value possible of a XOR b, and return that value.

--- problem_solving/python/algorithms/bit_manipulation/maximizing_xor.py
+++ problem_solving/python/algorithms/bit_manipulation/maximizing_xor.py
@@ -1,12 +1,27 @@
 def maximizing_xor(l, r):
-    xor_val = 0
-    for i in range(l, r+1):
-        for j in range(i, r+1):
-            xor = i ^ j
-            if xor > xor_val:
-                xor_val = xor
-    return xor_val
+    """Hackerrank Problem: https://www.hackerrank.com/challenges/maximizing-xor/problem
+
+    Given two integers, l and r, find the maximal value of a xor b, written a @ b, where a and b satisfy the following
+    condition:
+
+    l <= a <= b <= r
+
+    Solve:
+        We XOR the l and r bound and find the length of that integer in binary form. That gives us the binary from which
+        we can create the highest value of a xor b, because that falls within l and r.
+
+    Args:
+        l (int): an integer, the lower bound inclusive
+        r (int): an integer, the upper bound inclusive
+
+    Returns:
+        int: maximum value of the xor operations for all permutations of the integers from l to r inclusive
+    """
+    xor = l ^ r
+    xor_binary = "{0:b}".format(xor)
+    return pow(2, len(xor_binary)) - 1
 
 
 if __name__ == "__main__":
+    print(maximizing_xor(10, 15))
     print(maximizing_xor(11, 100))
