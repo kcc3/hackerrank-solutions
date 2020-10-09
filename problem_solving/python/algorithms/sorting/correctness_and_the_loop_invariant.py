4782bdbb9196bc1d546cca72bd4a4d3e448db560
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2020-10-08 18:29:56 -0700

    Update correctness_and_the_loop_invariant.py
    Fixed the typo in code, not catching comparison to index position 0

--- problem_solving/python/algorithms/sorting/correctness_and_the_loop_invariant.py
+++ problem_solving/python/algorithms/sorting/correctness_and_the_loop_invariant.py
@@ -46,7 +46,7 @@ def insertion_sort(l):
     for i in range(1, len(l)):
         j = i - 1
         key = l[i]
-        while (j > 0) and (l[j] > key):
+        while (j >= 0) and (l[j] > key):
             l[j + 1] = l[j]
             j -= 1
         l[j + 1] = key
